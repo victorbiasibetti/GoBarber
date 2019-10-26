b3f1cc2cefb9158cbdd10e92d1f7595b10ba27a6
Victor Biasibetti <victorbiasibetti7@gmail.com>
2019-10-26 17:43:52 -0300

    adição de campos de cancelavel e agend. passado

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -1,5 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
-import { isBefore } from 'date-fns';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   static init(sequelize) {
@@ -10,7 +10,13 @@ class Appointment extends Model {
         past: {
           type: Sequelize.VIRTUAL,
           get() {
-            isBefore(this.date, new Date());
+            return isBefore(this.date, new Date());
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
           },
         },
       },
