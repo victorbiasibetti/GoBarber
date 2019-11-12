3350aab83b663a5a5f5e1a9be400fdd81d8f740a
Victor Biasibetti <victorbiasibetti7@gmail.com>
2019-11-11 21:39:06 -0300

    ajustes para utilizar frontend

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 import Auth from '../../config/auth';
 
 class SessionController {
@@ -18,7 +19,16 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
     }
@@ -26,12 +36,14 @@ class SessionController {
     if (!(await user.checkPassword(password))) {
       return res.status(401).json({ error: 'password does not match' });
     }
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
     return res.json({
       user: {
         id,
         name,
         email,
+        avatar,
+        provider,
       },
       token: jwt.sign({ id }, Auth.secret, {
         expiresIn: Auth.expiresIn,
